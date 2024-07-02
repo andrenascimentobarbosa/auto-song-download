6383b6eb25ba954e062c35234d5edf5be9398539
andrenascimentobarbosa <andrebarbosa2024@gmail.com>
2024-07-02 16:11:50 -0300

    Working on it

--- src/autosong.py
+++ src/autosong.py
@@ -1,51 +1,38 @@
 #!/usr/bin/python3
 
-import os
-import sys
 from pytube import YouTube
-from pydub import AudioSegment
+import sys
 
 
-def donwload_video(youtube_link, output_folder='songs'):
+def Download(link):
+    yt = YouTube(link)
+    yt = yt.streams.get_highest_resolution()
     try:
-        # Created output folder in it doesn't exist
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
-
-        # Download the video
-        yt = YouTube(youtube_link)
-        video_stream = yt.streams.filter(only_audio=True).first()
-        video_path = video_stream.download(output_folder)
-
-        # Convert video to mp3
-        base, ext = os.path.splitext(video_path)
-        mp3_path = base + '.mp3'
-
-        # Use pydub to convert to mp3
-        audio = AudioSegment.from_file(video_path)
-        audio.export(mp3_path, format='mp3')
-        
-        # Remove the original video file
-        os.remove(vide_path)
-
-        print('Donwloaded and converted {yt.title} to mp3 successfully.')
+        yt.download()
     except Exception as e:
         print('Error:', e)
 
+    print('done.')
 
-def download_videos(songlist, output_folder='songs'):
-    with open(songlist, 'r') as f:
-        f.readlines()
-        for song in f:
-            download_video(f, output_folder)
+
+'''
+def main(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        for l in lines:
+            Download(l)
+'''
+
+
+def main(link):
+    Download(link)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print('./script.py songlist.txt')
-        sys.exit(1)
+    #if len(sys.argv) != 2:
+        #print('./script.py songlist.txt')
+    
+    #songs = sys.argv[1]
+    songs = 'https://youtu.be/-wZl_ZhnVg4?si=bdxI-JQQG5bI2pqT'
+    main(songs)
 
-    songs = sys.argv[1]
-    
-    download_videos(songs)
-    
